cc6acfb2aa71819586c6ea8967ad30e4357a36f3
Kent Rancourt <kent.rancourt@microsoft.com>
2022-01-26 11:37:23 -0500

    take advantage of github gateway's new ci/cd abstraction
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

--- .brigade/brigade.ts
+++ .brigade/brigade.ts
@@ -16,8 +16,8 @@ class MakeTargetJob extends Job {
   }
 }
 
-// A map of all jobs. When a check_run:rerequested event wants to re-run a
-// single job, this allows us to easily find that job by name.
+// A map of all jobs. When a ci:job_requested event wants to re-run a single
+// job, this allows us to easily find that job by name.
 const jobs: {[key: string]: (event: Event) => Job } = {}
 
 const testUnitJobName = "test-unit"
@@ -34,29 +34,21 @@ const lintJob = (event: Event) => {
 }
 jobs[lintJobName] = lintJob
 
-async function runSuite(event: Event): Promise<void> {
+events.on("brigade.sh/github", "ci:pipeline_requested", async event => {
   await new ConcurrentGroup( // Basic tests
     testUnitJob(event),
     lintJob(event),
   ).run()
-}
-
-// Either of these events should initiate execution of the entire test suite.
-events.on("brigade.sh/github", "check_suite:requested", runSuite)
-events.on("brigade.sh/github", "check_suite:rerequested", runSuite)
+})
 
 // This event indicates a specific job is to be re-run.
-events.on("brigade.sh/github", "check_run:rerequested", async event => {
-  // Check run names are of the form <project name>:<job name>, so we strip
-  // event.project.id.length + 1 characters off the start of the check run name
-  // to find the job name.
-  const jobName = JSON.parse(event.payload).check_run.name.slice(event.project.id.length + 1)
-  const job = jobs[jobName]
+events.on("brigade.sh/github", "ci:job_requested", async event => {
+  const job = jobs[event.labels.job]
   if (job) {
     await job(event).run()
     return
   }
-  throw new Error(`No job found with name: ${jobName}`)
+  throw new Error(`No job found with name: ${event.labels.job}`)
 })
 
 events.process()
